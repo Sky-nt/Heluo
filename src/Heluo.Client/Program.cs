bf2cb6a3c3971af9d8bf383babb2caf21fd7baaa
牛延瞳 <niutong1995@163.com>
2024-12-02 18:51:21 +0800

    初始化

---
+++ src/Heluo.Client/Program.cs
@@ -0,0 +1,14 @@
+using Heluo.Rcl;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMasaBlazor();
+
+await builder.Build().RunAsync();
---
+++ src/Heluo/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Heluo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
